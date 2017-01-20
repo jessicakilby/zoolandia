ae9a4c6be5a843deeadd201b7d4bf6ccf8fb9f12
jessica kilby <jessicakilby@gmail.com>
2017-01-19 18:49:29 -0600

    add IEatFood interface

--- PartOfTheWorld.cs
+++ PartOfTheWorld.cs
@@ -2,11 +2,23 @@ using System;
 
 namespace Zoolandia.Animals
 {
-    public class PartOfTheWorld
+    public class PartOfTheWorld : IEatFood
     {
         public static void theWorld ()
         {
             Console.WriteLine("If I go, so does everything inside me. Apocalypse!!");
+        }
+        public bool Eating()
+        {
+            return true;
+        }
+        public void NotEating()
+        {
+            Console.WriteLine("I'm not eating right now.");
+        }
+        public void EatingFood()
+        {
+            Console.WriteLine("I am eating food right now");
         }
     }
 }
