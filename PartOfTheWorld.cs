8883dddae6c737bc322feefe651e452369e6ba13
jessica kilby <jessicakilby@gmail.com>
2017-01-19 18:28:50 -0600

    refactored is-a, has-a and part-of relationships version 5 done

---
+++ PartOfTheWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Zoolandia.Animals
+{
+    public class PartOfTheWorld
+    {
+        public static void theWorld ()
+        {
+            Console.WriteLine("If I go, so does everything inside me. Apocalypse!!");
+        }
+    }
+}
